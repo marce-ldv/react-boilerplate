1c63cc76ece3b6961175796b43cffaab8fb24451
MarceloLudovino <ext-m.ludovino@rappi.com>
2019-12-23 12:46:48 -0300

    Add connect

--- src/containers/Pokemons/index.jsx
+++ src/containers/Pokemons/index.jsx
@@ -1,25 +1,3 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getPokemonList } from 'core/Pokemons/actions';
-import { getPokemonsSelector } from 'core/Pokemons/selectors';
-import Table from 'components/Table';
-import { pokeColumns } from './columns';
-
-const Pokemons = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPokemonList());
-  }, [dispatch]);
-
-  const pokemons = useSelector(state => getPokemonsSelector(state));
-
-  return (
-    <>
-      <h1>Pokemons:</h1>
-      <Table data={pokemons} columns={pokeColumns} />
-    </>
-  );
-};
+import Pokemons from './container';
 
 export default Pokemons;
--- src/core/axios.instance.js
+++ src/core/axios.instance.js
@@ -4,6 +4,7 @@ const headers = {};
 
 const instance = axios.create({
   baseURL: `https://pokeapi.co/api/v2/`,
+  headers,
 });
 
 export default instance;
