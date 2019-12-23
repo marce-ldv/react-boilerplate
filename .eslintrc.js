539d7b7b1c6c8b1534e45663795a6d299ba67827
MarceloLudovino <ext-m.ludovino@rappi.com>
2019-12-23 10:27:30 -0300

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es6": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:prettier/recommended"
+	],
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parserOptions": {
+		"ecmaFeatures": {
+			"jsx": true
+		},
+		"ecmaVersion": 2018,
+		"sourceType": "module"
+	},
+	"plugins": [
+		"react",
+		"react-hooks",
+	],
+	"rules": {
+		"react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+		"react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
+	}
+};
